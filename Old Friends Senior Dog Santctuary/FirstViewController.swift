512045bd439368653d11deb6ee40ca730675a326
Cal Stephens <cal@calstephens.tech>
2018-01-06 15:55:24 -0500

    Initial Commit

---
+++ Old Friends Senior Dog Santctuary/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Old Friends Senior Dog Santctuary
+//
+//  Created by Cal Stephens on 1/6/18.
+//  Copyright © 2018 Cal Stephens. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
