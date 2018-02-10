57b3037c26840b976c8d2728a10534d5cbfa4e7a
Cal Stephens <cal@calstephens.tech>
2018-02-10 13:34:41 -0500

    Hard-restart the player after five hours in the background to force it to refetch the stream URL

--- Old Friends Senior Dog Santctuary/LivestreamViewController.swift
+++ Old Friends Senior Dog Santctuary/LivestreamViewController.swift
@@ -11,27 +11,62 @@ import AVKit
 
 class LivestreamViewController: AVPlayerViewController {
     
+    // MARK: Lifecycle
+    
     override func viewWillAppear(_ animated: Bool) {
-        if let player = player {
-            if let live = player.currentItem?.seekableTimeRanges.last?.timeRangeValue {
-                player.seek(to: live.start + live.duration)
-            }
-            
-            player.play()
-            
-            self.showsPlaybackControls = false
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                self.showsPlaybackControls = true
-            }
-            
+        if player == nil {
+            setUpPlayer()
         } else {
-            livestream.play(in: self)
+            unpausePlayer()
         }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
+        pausePlayer()
+    }
+    
+    // MARK: Player setup
+    
+    private var timePaused: Date?
+    
+    func setUpPlayer() {
+        livestream.play(in: self)
+    }
+    
+    func pausePlayer() {
+        timePaused = Date()
+        
         player?.pause()
         showsPlaybackControls = false
+    }
+    
+    func unpausePlayer() {
+        guard let player = player else { return }
+        
+        // if the player has been paused (aka in the background) for more than 5 hours,
+        // restart the player and refetch the livestream URL to prevent
+        // the player from getting stuck on an outdated URL
+        let timeSincePaused = Date().timeIntervalSince(timePaused ?? Date())
+        if timeSincePaused > (5*60*60) {
+            restartPlayer()
+            return
+        }
+        
+        if let live = player.currentItem?.seekableTimeRanges.last?.timeRangeValue {
+            player.seek(to: live.start + live.duration)
+        }
+        
+        player.play()
+        
+        self.showsPlaybackControls = false
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.showsPlaybackControls = true
+        }
+    }
+    
+    func restartPlayer() {
+        self.player = nil
+        setUpPlayer()
     }
     
     // MARK: Subclass customization points
